32b1e6e493001be0fd19036b18a8d917c1432fce
Ante Botić <115645763+boticante@users.noreply.github.com>
2025-06-08 00:52:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
-        ? true
-        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+        ? "https://chatzavrsni.onrender.com/" 
+        : ["http://localhost:5500/", "http://127.0.0.1:5500/"],
   },
 });
 
